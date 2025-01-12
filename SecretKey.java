8783148c40784f113989ca3607d4e298654493c0
itsnotkamana <kamanarmtl08@gmail.com>
2025-01-12 17:39:31 +0545

    include

--- SecretKey.java
+++ SecretKey.java
@@ -1,5 +1,10 @@
 package tutorial;
 
 public class SecretKey {
+	private static final String SECRET_KEY = "YourSecretKeyHere";
+
+    public static String getSecretKey() {
+        return SECRET_KEY;
+    }
 
 }
