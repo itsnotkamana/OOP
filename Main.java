350ff62d8f1cdcf8fad0678e22891110c7e47e4b
itsnotkamana <kamanarmtl08@gmail.com>
2025-01-13 08:10:15 +0545

    Add Main.java, Student.java, GradeCalculator.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+package tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Student Management System");
+    }
+}
---
+++ Student.java
@@ -0,0 +1,26 @@
+package tutorial;
+
+public class Student {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', age=" + age + "}";
+    }
+}
